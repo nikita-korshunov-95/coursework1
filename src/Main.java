09016794860a145aa4ffc482f6998c09a9f4460c
Nikita <nikita.korshunov.95@mail.ru>
2024-01-03 23:48:24 +0500

    coursework1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Storage storage = new Storage();
+        storage.printAllEmployee();
+        System.out.println("Фонд отплаты труда: " + storage.calculateAllSalary());
+        System.out.println("Сотрудник с минимальной зарплатой: " + storage.calculateMinEmployee());
+        System.out.println("Сотрудник с максимальной зарплатой: " + storage.calculateMaxEmployee());
+        System.out.println("Средняя зарплата офиса: " + storage.calculateMediumSalary());
+        storage.printFullName();
     }
 }
